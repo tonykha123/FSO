33f757c7005fddeefa6b436dfb085de9f1378c33
Tony K <tonykha.1705@gmail.com>
2022-08-30 14:30:49 +1200

    7.7

--- Part7 /country-hook/src/hooks/index.js
+++ Part7 /country-hook/src/hooks/index.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-
 export const useField = (type) => {
   const [value, setValue] = useState('')
 
@@ -16,11 +15,27 @@ export const useField = (type) => {
   }
 }
 
+//useCountry custom hook takes in a name
+//it then uses that name passed through to do a fetch to the api endpoint
+//https://restcountries.com/v3.1/name/{name}?fullText=true
+//then sets country which is initally null
+//to the returned array list of countries
 export const useCountry = (name) => {
   const [country, setCountry] = useState(null)
 
-  useEffect(() => {})
+  //useEffect kicks everytime name changes as it is the depedency
+  useEffect(() => {
+    if (name) {
+      axios
+        .get(`https://restcountries.com/v3.1/name/${name}?fullText=true`)
+        .then((response) => {
+          setCountry(response.data[0])
+        })
+        .catch((err) => {
+          setCountry(null)
+        })
+    }
+  }, [name])
 
   return country
 }
-
