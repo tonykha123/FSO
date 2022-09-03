d885c1afc6f9d6b40e387840496fb0f5b3c6fba6
Tony K <tonykha.1705@gmail.com>
2022-09-03 15:59:14 +1200

    8.2

--- Part8/library-backend/index.js
+++ Part8/library-backend/index.js
@@ -1,1 +1,30 @@
 const { ApolloServer, gql } = require('apollo-server')
+const { books, authors } = require('./db')
+
+const typeDefs = gql`
+  type Book {
+    title: String!
+    author: String!
+    genres: [String!]!
+    id: ID!
+  }
+  type Query {
+    bookCount: Int!
+    authorCount: Int!
+    allBooks: [Book!]!
+  }
+`
+console.log(books)
+const resolvers = {
+  Query: {
+    bookCount: () => books.length,
+    authorCount: () => authors.length,
+    allBooks: () => books,
+  },
+}
+
+const server = new ApolloServer({ typeDefs, resolvers })
+
+server.listen().then(({ url }) => {
+  console.log(`listening on ${url}`)
+})
