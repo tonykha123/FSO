d7e2d7ffaab93e719bca651d8a384fb91b1de5ea
Tony K <tonykha.1705@gmail.com>
2022-09-15 08:27:56 +1200

    done command line argument bmi calculator

--- Part9/First Steps/bmiCaclulator.ts
+++ Part9/First Steps/bmiCaclulator.ts
@@ -1,3 +1,22 @@
+interface BmiValues {
+  value1Weight: number
+  value2Height: number
+}
+
+const parseBmiArgs = (args: Array<string>): BmiValues => {
+  if (args.length < 4) throw new Error('Not enough arguments')
+  if (args.length > 4) throw new Error('Too many arguments')
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      value1Weight: Number(args[2]),
+      value2Height: Number(args[3]),
+    }
+  } else {
+    throw new Error('Provided values must be a number')
+  }
+}
+
 const bmiCalculator = (weightInKg: number, heightInCm: number): string => {
   const heightSquared = (heightInCm * heightInCm) / 10000
   const bmi = weightInKg / heightSquared
@@ -11,4 +30,14 @@ const bmiCalculator = (weightInKg: number, heightInCm: number): string => {
   } else return `Your Bmi is ${bmi}, you are in the obese range`
 }
 
-console.log(bmiCalculator(700, 170))
+try {
+  //destructures return object from function
+  const { value1Weight, value2Height } = parseBmiArgs(process.argv)
+  console.log(bmiCalculator(value1Weight, value2Height))
+} catch (error: unknown) {
+  let errorMessage = 'something went wrong: '
+  if (error instanceof Error) {
+    errorMessage += `Error: ${error.message}`
+  }
+  console.log(errorMessage)
+}
