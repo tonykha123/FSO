644193a12fcbbfa6ba7cf9934c99dc8d7117fea2
Tony K <tonykha.1705@gmail.com>
2022-09-23 18:31:25 +1200

    9.17

--- Part9/Patientor/FrontEnd/src/IndividualPatient/IndividualPatient.tsx
+++ Part9/Patientor/FrontEnd/src/IndividualPatient/IndividualPatient.tsx
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
+import { Patient } from "../types";
+import { apiBaseUrl } from "../constants";
+import { useStateValue } from "../state";
+import { useParams } from "react-router-dom";
 
 const IndividualPatient = () => {
+  const { id } = useParams<{ id: string }>();
+  const [{ patients }, dispatch] = useStateValue();
+  if (!id || !patients) return null;
+  const foundPatient = patients[id];
+  if (!foundPatient) return null;
+
+  useEffect(() => {
+    const fetchSinglePatient = async () => {
+      try {
+        if (id) {
+          const { data: singlePatient } = await axios.get<Patient>(
+            `${apiBaseUrl}/patients/${id}`
+          );
+          dispatch({ type: "UPDATE_PATIENT", payload: singlePatient });
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    void fetchSinglePatient();
+  }, []);
+
   return (
     <div>
-      IndividualPatient
-      <h1>hey</h1>
+      <h1>Name : {foundPatient.name}</h1>
+      <h3>ssn : {foundPatient.ssn}</h3>
+      <h3> Occupation : {foundPatient.occupation}</h3>
     </div>
   );
 };
--- Part9/Patientor/FrontEnd/src/state/reducer.ts
+++ Part9/Patientor/FrontEnd/src/state/reducer.ts
@@ -9,6 +9,10 @@ export type Action =
   | {
       type: "ADD_PATIENT";
       payload: Patient;
+    }
+  | {
+      type: "UPDATE_PATIENT";
+      payload: Patient;
     };
 
 export const reducer = (state: State, action: Action): State => {
@@ -21,17 +25,26 @@ export const reducer = (state: State, action: Action): State => {
             (memo, patient) => ({ ...memo, [patient.id]: patient }),
             {}
           ),
-          ...state.patients
-        }
+          ...state.patients,
+        },
       };
     case "ADD_PATIENT":
       return {
         ...state,
         patients: {
           ...state.patients,
-          [action.payload.id]: action.payload
-        }
+          [action.payload.id]: action.payload,
+        },
       };
+
+      case "UPDATE_PATIENT":
+        return {
+          ...state,
+          patients: {
+            ...state.patients,
+            [action.payload.id]: action.payload,
+          },
+        };
     default:
       return state;
   }
