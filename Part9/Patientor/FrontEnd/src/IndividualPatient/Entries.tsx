3424699aca5cebf791f9b19b44d46ab016249e46
Tony K <tonykha.1705@gmail.com>
2022-10-07 10:48:51 +1300

    9.21

--- Part9/Patientor/FrontEnd/src/IndividualPatient/Entries.tsx
+++ Part9/Patientor/FrontEnd/src/IndividualPatient/Entries.tsx
@@ -9,21 +9,21 @@ const Entries = ({ patient }: IProps) => {
 
   if (!patient.entries) return <div>LOADING</div>;
 
-
+  const entries = patient.entries.map((e) => (
+    <div key={e.id}>
+      {e.date} {e.description}
+      <ul>
+        {e.diagnosisCodes?.map((c) => (
+          <li key={c}>{c}</li>
+        ))}
+      </ul>
+    </div>
+  ));
 
   return (
     <div>
       <h1>Entries</h1>
-      {patient.entries.map((e) => (
-        <div key={e.id}>
-          {e.date} {e.description}
-          <ul>
-            {e.diagnosisCodes?.map((c) => (
-              <li key={c}>{c}</li>
-            ))}
-          </ul>
-        </div>
-      ))}
+      {entries}
     </div>
   );
 };
